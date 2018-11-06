3df51941a8d89f19925cc5f128f13c2c603255c3
levanliang <973434891@qq.com>
2018-11-06 20:55:27 +0800

    检测重读文本

--- src/program.go
+++ src/program.go
@@ -1,11 +1,11 @@
 package main
 
-
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
-
 
 func printArgs() {
 	for i := 1; i < len(os.Args); i++ {
@@ -14,16 +14,40 @@ func printArgs() {
 }
 
 func printArgsRange() {
-	for _, arg = range os.Args[1:] {
+	for _, arg := range os.Args[1:] {
 		fmt.Println(arg)		
 	}
 }
 
+func printArgsJoin() {
+	fmt.Println(strings.Join(os.Args[1:], ";"))
+}
+
+func dup() {
+	counter := make(map[string]int)
+	for _, filename := os.Args[1:] {
+		text, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println("open ", filename, " failed!")
+			continue;
+		}
+		for _, line := range strings.Split(string(text), "\n") {
+			counter[line]++
+		}
+	}
+	for line, num := range counter {
+		if num > 1 {
+			fmt.Println(line, " dup!")
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Hello, golang!");
 
-	printArgs()
-	printArgsRange()
+	//printArgs()
+	//printArgsRange()
+	//printArgsJoin()
+	dup()
 }
 
