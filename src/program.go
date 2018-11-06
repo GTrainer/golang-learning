659301daed6ea6195de3df4d483b5a809271e5fa
levanliang <973434891@qq.com>
2018-11-06 20:22:11 +0800

    打印命令行参数

--- src/program.go
+++ src/program.go
@@ -3,10 +3,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+
+func printArgs() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Println(os.Args[i])
+	}	
+}
 
 
 func main() {
 	fmt.Println("Hello, golang!");
+
+	printArgs()
 }
 
